c0cce9c6d2111001eeb6b20ebe081e1e3cb52693
Faizan Ejaz <xheikhfazi007@gmail.com>
2022-08-29 20:54:42 +0500

    That Code

---
+++ NET_QR/ViewComponenets/EmailQr.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NET_QR.ViewComponenets
+{
+    public class EmailQr : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("Default");
+        }
+    }
+}
---
+++ NET_QR/ViewComponenets/SmsQr.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NET_QR.ViewComponenets
+{
+    public class SmsQr : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("Default");
+        }
+    }
+}
